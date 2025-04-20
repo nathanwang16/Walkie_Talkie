f38d133ba27b4243acf3ccee0156c630c9ab9f17
Nathan Wang <nathan16@foxmail.com>
2025-04-19 23:16:35 -0700

    Initial Commit

---
+++ Walkie_Talkie/Walkie_TalkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Walkie_TalkieApp.swift
+//  Walkie_Talkie
+//
+//  Created by Nathan Wang on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Walkie_TalkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Walkie_TalkieTests/Walkie_TalkieTests.swift
@@ -0,0 +1,17 @@
+//
+//  Walkie_TalkieTests.swift
+//  Walkie_TalkieTests
+//
+//  Created by Nathan Wang on 4/19/25.
+//
+
+import Testing
+@testable import Walkie_Talkie
+
+struct Walkie_TalkieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
